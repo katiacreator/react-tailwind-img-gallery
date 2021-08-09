d346c280e731949fed0b10b727bda8735746eca8
katiacreator <katia.creator@gmail.com>
2021-08-08 22:24:58 -0400

    dynamicalled added tags

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 
 const ImageCard = ({ image }) => {
+
+  const tags = image.tags.split(',')
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+    <div className="max-w-sm rounded overflow-hidden shadow-lg my-4">
     <img src={image.webformatURL} alt="" className="w-full" />
     <div className="px-6 py-4">
       <div className=" font-bold text-purple-500 text-xl">Photo by {image.user}</div>
@@ -22,9 +24,9 @@ const ImageCard = ({ image }) => {
       </ul>
     </div>
     <div className="px-6 py-2">
-      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag1</span>
-      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag2</span>
-      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag3</span>
+     {tags.map(tag =>(
+        <span key={image.id} className="inline-block bg-gray-200 rounded-full px-3 py-1 my-1 text-sm font-semibold text-gray-700 mr-2">#{tag}</span>
+     ))}
     </div>
   </div>
   )

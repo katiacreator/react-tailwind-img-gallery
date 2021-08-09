25a5ed7db9607936971144e5bec12d9f8734bb08
katiacreator <katia.creator@gmail.com>
2021-08-08 21:07:36 -0400

    created initial cstatic card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <img src="https://source.unsplash.com/random" alt="" className="" />
+      <div className="px-6 py-4">
+        <div className=" font-bold text-purple-500 text-xl">Photo by John Doe</div>
+      </div>
+      <div>
+        <ul>
+          <li>
+            <p className="px-6 py-1">Views: 4000</p> 
+          </li>
+          <li>
+            <p className="px-6 py-1">Downloads: 400</p> 
+          </li>
+          <li>
+            <p className="px-6 py-1">Likes: 400</p> 
+          </li>
+        </ul>
+      </div>
+      <div className="px-6 py-2">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag1</span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag2</span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag3</span>
+      </div>
     </div>
   );
 }
